8c423358a97e59c555cea94a0cfcb717540ff9b0
sai-ma <skhali202@caledonian.ac.uk>
2021-03-15 02:45:39 +0000

    routes commit

---
+++ routes/trainingRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const controller = require('../controllers/trainingControllers');
+const router = express.Router();
+
+
+
+router.get('/', controller.landing_page ); 
+
+
+router.get('/guestbook', controller.trainingLog_page);
+
+//for static files, have to 'redirect' and add file path...?
+router.get('/about', controller.about_page);
+
+router.get('/new', controller.new_entry); 
+
+router.use(controller.not_found);
+
+module.exports = router;
